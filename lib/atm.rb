c8d85f31a5feba3a3d85f95bbb244f3b45194430
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-22 10:07:53 +0200

    added insuffient funds in ATM

--- lib/atm.rb
+++ lib/atm.rb
@@ -8,7 +8,9 @@ class Atm
   def withdraw(amount, account)
     case
     when insufficient_funds_in_account?(amount,account)
-        { status: false, message: 'insufficient funds', date: Date.today }
+      { status: false, message: 'insufficient funds', date: Date.today }
+    when insufficient_funds_in_atm?(amount)
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
       else
         perform_transaction(amount, account)
       end
@@ -23,5 +25,9 @@ class Atm
       @funds -= amount
       account.balance = account.balance - amount
       { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+
+    def insufficient_funds_in_atm?(amount)
+      @funds < amount
     end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -15,7 +15,12 @@ describe Atm do
       expected_output = { status: false, message: 'insufficient funds', date: Date.today }
       expect(subject.withdraw(105, account)).to eq expected_output
     end
-  it 'has 1000$ on initialize' do
+    it 'reject withdraw if ATM has insufficient funds' do
+      subject.funds = 50
+      expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
+      expect(subject.withdraw(100, account)). to eq expected_output
+    end
+    it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
   it 'funds are reduced at withdraw' do

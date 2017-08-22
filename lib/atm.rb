894815544280d941d107de70c305808f6c74421f
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-22 15:25:45 +0200

    account disable

--- lib/atm.rb
+++ lib/atm.rb
@@ -13,6 +13,10 @@ class Atm
       { status: false, message: 'insufficient funds in ATM', date: Date.today }
     when incorrect_pin?(pin_code, account.pin_code)
       { status: false, message: 'wrong pin', date: Date.today }
+    when card_expired?(account.exp_date)
+      { status: false, message: 'card_expired', date: Date.today }
+    when account_disabled?(account.account_status)
+      { status: false, message: 'account disabled', date: Date.today }
       else
         perform_transaction(amount, account)
       end
@@ -38,4 +42,11 @@ class Atm
     def incorrect_pin?(pin_code, actual_pin)
       pin_code != actual_pin
     end
-  
+
+    def card_expired?(exp_date)
+      Date.strptime(exp_date, '%m/%y') < Date.today
+    end
+
+    def account_disabled?(account_status)
+      account_status != :active
+    end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm.rb'
 require 'date'
 
 describe Atm do
-  let(:account) { instance_double('Account', pin_code: '1234') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '10/17', account_status: :active) }
   before do
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
@@ -23,6 +23,16 @@ describe Atm do
     it 'reject withdraw if pin is wrong' do
       expected_output = { status: false, message: 'wrong pin', date: Date.today }
       expect(subject.withdraw(50, 9999, account)).to eq expected_output
+    end
+    it 'reject withdraw if card is expired' do
+      allow(account).to receive(:exp_date).and_return('12/15')
+      expected_output = { status: false, message: 'card_expired', date: Date.today }
+      expect(subject.withdraw(6, '1234', account)).to eq expected_output
+    end
+    it 'reject withdraw if account is disabled' do
+      allow(account).to receive(:account_status).and_return(:disabled)
+      expected_output = { status: false, message: 'account disabled', date: Date.today }
+      expect(subject.withdraw(10, '1234', account)). to eq expected_output
     end
     it 'has 1000$ on initialize' do
       expect(subject.funds).to eq 1000

f4662e1e3117cbb91a89027cb915fa73314d32f9
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-22 15:56:33 +0200

    added cash to the atm

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,7 +8,7 @@ describe Atm do
     allow(account).to receive(:balance=)
   end
     it 'allow withdraw if account has enough balance.' do
-      expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+      expected_output = { status: true, message: 'success', date: Date.today, amount: 45, bills: [20, 20, 5] }
       expect(subject.withdraw(45, '1234', account)).to eq expected_output
     end
     it 'rejects withdraw if account has insufficient funds' do

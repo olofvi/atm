b7f5cb194b020172c613e35afbb866217846a1af
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-23 22:23:57 +0200

    ATM is working, refactoring left

--- lib/person.rb
+++ lib/person.rb
@@ -1,27 +1,57 @@
 require './lib/account.rb'
-require '.lib/atm.rb'
+require './lib/atm.rb'
+require 'pry'
 
 class Person
   attr_accessor :name, :cash, :account
 
-    def initialize(attrs = {})
-      set_name(attrs[:name])
-      @cash = 0
-    end
+  def initialize(attrs = {})
+    set_name(attrs[:name])
+    @cash = 0
+  end
 
-    def create_account(attrs = {})
-      @account = Account.new(owner: self)
-    end
+  def create_account(attrs = {})
+    @account = Account.new(owner: self)
+  end
 
-    def deposit
-      @balance = []
-
-  private
   def set_name(obj)
-obj == nil ?  missing_name : @name = obj
+    obj == nil ?  missing_name : @name = obj
+  end
+
+  def deposit(amount)
+    @account == nil ? missing_account : deposit_funds(amount)
+  end
+
+  def withdraw_funds(args)
+    args[:atm] == nil ? missing_atm : atm = args[:atm]
+    account = @account
+    amount = args[:amount]
+    pin = args[:pin]
+    response = atm.withdraw(amount, pin, account)
+    response[:status] == true ? increase_cash(response) : response
+  end
+  
+  private
+
+  def deposit_funds(amount)
+    @cash -= amount
+    @account.balance += amount
+  end
+
+  def increase_cash(response)
+    @cash += response[:amount]
+  end
+
+  def missing_atm
+    raise RuntimeError, 'An ATM is required'
+  end
+
+  def missing_account
+    raise "No account present"
   end
 
   def missing_name
+    @name= nil
     raise "A name is required"
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,46 +1,56 @@
 require './lib/person.rb'
 require './lib/atm.rb'
+require 'pry'
 
 describe Person do
   subject { described_class.new(name: 'Thomas') }
+
   it 'is expected to have a :name on initialize' do
     expect(subject.name).not_to be nil
   end
+
   it 'is expected to raise error if no name is set' do
     expect { described_class.new }.to raise_error 'A name is required'
   end
+
   it 'is expected to have a :cash attribute with value of 0 on initialize' do
     expect(subject.cash).to eq 0
   end
+
   it 'is expected to have a :account attribute' do
     expect(subject.account).to be nil
   end
-describe 'can create an Account' do
-  before { subject.create_account }
-  it 'of Account class ' do
-    expect(subject.account).to be_an_instance_of Account
+
+  describe 'can create an Account' do
+    before { subject.create_account }
+
+    it 'of Account class ' do
+      expect(subject.account).to be_an_instance_of Account
+    end
+
+    it 'with himself as an owner' do
+      expect(subject.account.owner).to be subject
+    end
   end
-  it 'with himself as an owner' do
-    expect(subject.account.owner).to be subject
-  end
+
+  describe 'can manage funds if an account been created' do
+    let(:atm) { Atm.new }
+
+    before { subject.create_account }
+    it 'can deposit funds' do
+      expect(subject.deposit(100)).to be_truthy
+    end
+    it 'funds are added to the accounst balance - deducted from cash' do
+      subject.cash = 100
+      subject.deposit(100)
+      expect(subject.account.balance).to be 100
+      expect(subject.cash).to be 0
+    end
 end
 
-describe 'can manage funds if an account been created' do
-  let(:atm) { Atm.new }
-  # As a Person with a Bank Account,
-  # in order to be able to put my funds in the account ,
-  # i would like to be able to make a deposit
-  before { subject.create_account }
-  it 'can deposit funds' do
-    expect(subject.deposit(100)).to be_truthy
+  describe 'can not manage funds if no account been created' do
+    it 'can\'t deposit funds' do
+      expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
+    end
   end
-
-# describe 'can not manage funds if no account been created' do
-#   # As a Person without a Bank Account,
-#   # in order to prevent me from using the wrong bank account,
-#   # I should NOT be able to to make a deposit.
-#   it 'can\'t deposit funds' do
-#     expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
-#   end
-# end
 end

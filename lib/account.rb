d26ea94064b00cf7d60b5adc055d5aa6f53df44f
olofvi <30499007+olofvi@users.noreply.github.com>
2017-08-23 13:06:29 +0200

    finished the account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,26 @@
 require 'Date'
 
 class Account
-  attr_accessor :pin_code
-  # attr_accessor :balance
-  attr_accessor :account_status
-  attr_accessor :exp_date
-  # attr_accessor :account_owner
-
-  STANDARD_VALIDITY_YEARS = 5
-end
+    STANDARD_VALIDITY_YEARS = 5
+  attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
+    def deactivate
+      @account_status = :deactivated
+    end
 
   def initialize(attrs = {})
     @pin_code = rand(1000..9999)
     @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YEARS).strftime('%m/%y')
     @account_status = :active
+    @balance = 0
+    set_owner(attrs[:owner])
   end
+
+  private
+    def set_owner(obj)
+    obj == nil ?  missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise "An Account owner is required"
+  end
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,18 @@ require './lib/account.rb'
 require 'spec_helper'
 
 describe Account do
+  let(:person) {instance_double('Person', name: 'Thomas')}
+  subject { described_class.new({owner: person}) }
+
+  it 'is expected to have an owner' do
+    expect(subject.owner).to eq person
+  end
+  it 'is expected to raise error if no owner is set' do
+    expect { described_class.new }.to raise_error 'An Account owner is required'
+    end
+  it 'is expected to have a balance of a 0 on initialize' do
+    expect(subject.balance).to eq 0
+  end
   it 'check length of a number' do
     number = 1234
     number_length = Math.log10(number).to_i + 1
@@ -13,5 +25,9 @@ describe Account do
   end
   it 'is expected to have: active status on initialize' do
     expect(subject.account_status).to eq :active
+  end
+  it 'deactivates account using Instance method' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
   end
 end
